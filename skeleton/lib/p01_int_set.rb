e3f7dc12f0ef96970dcf241116a6c7184c463ae8
Marvin Ma <yma010@ucr.edu>
2019-08-14 16:17:10 -0700

    Initial Commit

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,103 @@
+require "byebug"
+
+class MaxIntSet
+  attr_reader :store, :max
+
+  def initialize(max)
+    @store = Array.new(max, false)
+    @max = max
+  end
+
+  def insert(num)
+    range = (0..max).to_a
+    raise "Out of bounds" unless range.include?(num)
+    store[num] = true 
+  end
+
+  def remove(num)
+    store[num] = false
+  end
+
+  def include?(num)
+    store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  attr_reader :store
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num 
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end 
+
+  private
+
+  def [](num)
+    store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < count
+  end
+
+  def remove(num)
+    @count -= 1 if self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    new_store = @store
+    @count = 0
+    @store = Array.new(2 * num_buckets) { Array.new }
+    new_store.flatten.each {|el| insert(el)}
+  end
+end
